ecdb23e5e0b7879718df0279725a1325d84097a7
HzDSN <i@hzdsn.me>
2016-10-24 14:17:37 +0800

    杨辉三角

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,14 +10,29 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[100];
-            for (int i = 1; i <= 100; i++)
+            int i, j;
+            int[,] array = new int[15, 15];
+            array[1, 1] = array[1, 2] = 1;
+            Console.WriteLine(1);
+            Console.WriteLine("1 1");
+            for (i = 2; i <= 10; i++)
             {
-                array[i - 1] = i;
-                Console.Write(array[i - 1]);
-                Console.Write(" ");
+                for (j = 1; j <= i + 1; j++)
+                {
+                    if (j == 1)
+                    {
+                        array[i, j] = 1;
+                        Console.Write("1");
+                    }
+                    else
+                    {
+                        array[i, j] = array[i - 1, j] + array[i - 1, j - 1];
+                        Console.Write(array[i, j]);
+                    }
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
             }
-            Console.ReadLine();
         }
     }
 }
