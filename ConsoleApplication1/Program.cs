fa0a28851a165e3478b87c0ee80b5b8dfba2ad09
HzDSN <i@hzdsn.me>
2016-10-24 14:03:23 +0800

    HzDSN

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            for (int i = 1; i <= 100; i++)
+            {
+                array[i - 1] = i;
+                Console.Write(array[i - 1]);
+                Console.Write(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
